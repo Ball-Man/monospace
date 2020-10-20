cd1a288772fe37f1cc30eb82fcae4eb01d9665d0
BallMan <francesco.mistri98@gmail.com>
2020-10-20 23:01:08 +0200

    New database migration: events and ships
    Also added the halloween event, with an event ship: the halloween ship.

--- monospace/migration.py
+++ monospace/migration.py
@@ -15,7 +15,7 @@ main_db_path = None
 
 APP_DB_PATH = app_storage_path() if monospace.on_android else None
 
-MAIN_DB_VERSION = 1      # PRAGMA user_version
+MAIN_DB_VERSION = 2      # PRAGMA user_version
 
 GET_VER_QUERY = 'PRAGMA user_version'
 SET_VER_QUERY = 'PRAGMA user_version=?'
@@ -65,11 +65,64 @@ def dump_main_db(overwrite=False):
 def add_movement_ratio(db):
     """Apply a patch and add the 'movement_ratio' option."""
     cursor = db.cursor()
-    cursor.execute("INSERT INTO `options`(option_name, value) "
+    cursor.execute("INSERT INTO `options`(`option_name`, `value`) "
                    + "VALUES('movement_ratio', 0)")
     db.commit()
 
 
+def add_ships_and_events(db):
+    """Apply a patch and add the needed tables for multiship management.
+
+    Also add a setting(the currently selected ship) and the halloween event
+    along with an halloween event ship.
+    """
+    cursor = db.cursor()
+
+    # New tables
+    cursor.execute(
+        "CREATE TABLE `events`(`id` INTEGER PRIMARY KEY AUTOINCREMENT,"
+        "`name` TEXT, `from_month` INTEGER, `from_day` INTEGER,"
+        "`to_month` INTEGER, `to_day` INTEGER)")
+
+    cursor.execute(
+        "CREATE TABLE `ships`(`name` TEXT PRIMARY KEY,"
+        "`unlocked` INTEGER)")
+
+    cursor.execute(
+        "CREATE TABLE `event_ships`("
+        "`ship_name` TEXT,"
+        "`event_id` INTEGER,"
+        "PRIMARY KEY (`ship_name`, `event_id`),"
+        "FOREIGN KEY (`ship_name`) REFERENCES `ships`(`name`),"
+        "FOREIGN KEY (`event_id`) REFERENCES `events`(`id`))")
+
+    cursor.execute(
+        "CREATE TABLE `point_ships`("
+        "`ship_name` TEXT PRIMARY KEY,"
+        "`total_points` INTEGER,"
+        "FOREIGN KEY (`ship_name`) REFERENCES `ships`(`name`))")
+
+    # New option (selected_ship)
+    cursor.execute("INSERT INTO `options`(`option_name`, `value`) "
+                   "VALUES('selected_ship', 'default')")
+
+    # Halloween event
+    cursor.execute(
+        "INSERT INTO events(name, from_month, from_day, to_month, to_day) "
+        "VALUES('halloween', 10, 21, 11, 10)")
+
+    # Halloween ship
+    cursor.execute(
+        "INSERT INTO `ships`(`name`, `unlocked`) VALUES('halloween_ship', 0)")
+
+    cursor.execute(
+        "INSERT INTO `event_ships`(`ship_name`, `event_id`) "
+        "VALUES('halloween_ship', 1)")
+
+    db.commit()
+
+
 VERSION_UPGRADES = {
-    1: add_movement_ratio
+    1: add_movement_ratio,
+    2: add_ships_and_events
 }
